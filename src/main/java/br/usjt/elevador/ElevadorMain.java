814ae2a7f6a7b90bdf7ae52eac7f0a8dd8da234f
Kaua Sousa <kauasantos2014s10@gmail.com>
2024-04-15 21:39:40 -0300

    Versão inicial

---
+++ src/main/java/br/usjt/elevador/ElevadorMain.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.usjt.elevador;
+
+/**
+ *
+ * @author 824149388
+ */
+public class ElevadorMain {
+    public static void main(String[] args) {
+        TelaElevador tela = new TelaElevador();
+        tela.setVisible(true);
+    }
+}
